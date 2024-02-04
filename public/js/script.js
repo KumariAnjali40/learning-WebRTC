a4e4fcc3adb4c9939627523f5629929c2d6597e2
KumariAnjali40 <142905766+KumariAnjali40@users.noreply.github.com>
2024-02-04 14:44:26 +0530

    get user media done

--- public/js/script.js
+++ public/js/script.js
@@ -2,14 +2,31 @@ var videoChatForm=document.getElementById('video-chat-form');
 var videoChatRoom=document.getElementById('video-chat-rooms');
 var joinBtn=document.getElementById('join');
 var roomName=document.getElementById('roomName')
-var userVideo=document.getElementById('user-video')
-var peerVideo=document.getElementById('peer-video');
+var userVideo=document.getElementById('user-video')//our video streaming.
+var peerVideo=document.getElementById('peer-video');//another side user.
 
 joinBtn.addEventListener("click",function(){
     if(roomName.value==""){
         alert("please enter a room name!")
     }else{
-        navigator.getUserMedia=navigator.getUserMedia;
-        
+        navigator.getUserMedia=navigator.getUserMedia ||navigator.webkitGetUserMedia || navigator.mozGetUserMedia;
+        navigator.getUserMedia(
+            {
+               //in this object here audio and video.
+               audio:true,
+               video:{width:1280,height:720}
+            },
+            function(stream){
+             //in this function video streaming 
+             videoChatForm.style="display:none";
+             userVideo.srcObject=stream;
+             userVideo.onloadedmetadata=function(e){
+                userVideo.play();
+             }
+            },
+            function(error){
+               alert("You can't access Media")
+            }
+        )
     }
 })
